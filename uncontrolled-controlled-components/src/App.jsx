a0839c16f245b509f229d5ca253b27521b7615ad
T14g <ottiagos@gmail.com>
2025-07-07 19:06:13 -0300

    feat: adiciona componente UncontrolledFlow para fluxo de passos não controlado

--- uncontrolled-controlled-components/src/App.jsx
+++ uncontrolled-controlled-components/src/App.jsx
@@ -1,36 +1,43 @@
 import './App.css'
-import UncontrolledForm from './components/UncontrolledForm';
-import ControlledForm from './components/ControlledForm';
-import UncontrolledModal from './components/UncontrolledModal';
-import ControlledModal from './components/ControlledModal';
-import React, { useRef, useState } from 'react';
+import { UncontrolledFlow } from './components/UncontrolledFlow'
 
-function App() {
-  const modalRef = useRef();
-  const [showControlledModal, setShowControlledModal] = useState(false);
-
+const StepOne = ({goNext}) => {
   return (
-    <div style={{ maxWidth: 500, margin: '40px auto', padding: 24, background: '#fafafa', borderRadius: 8, boxShadow: '0 2px 8px #0001' }}>
-      <h1>Exemplos de Componentes React</h1>
-      <section style={{ marginBottom: 32 }}>
-        <h2>Controlled Form</h2>
-        <ControlledForm />
-      </section>
-      <section style={{ marginBottom: 32 }}>
-        <h2>Uncontrolled Form</h2>
-        <UncontrolledForm />
-      </section>
-      <section style={{ marginBottom: 32 }}>
-        <h2>Uncontrolled Modal</h2>
-        <UncontrolledModal/>
-      </section>
-      <section>
-        <h2>Controlled Modal</h2>
-        <button onClick={() => setShowControlledModal(true)} style={{ padding: '8px 16px', fontSize: 16 }}>Show Controlled Modal</button>
-        <ControlledModal open={showControlledModal} onClose={() => setShowControlledModal(false)} />
-      </section>
+    <div>
+      <h1>I'm step one</h1>
+      <button onClick={goNext}>Next</button>
     </div>
   )
 }
+const StepTwo = ({goNext}) => {
+  return (
+    <div>
+      <h1>I'm step two</h1>
+      <button onClick={goNext}>Next</button>
+    </div>
+  )
+}
+const StepThree = ({goNext}) => {
+  return (
+    <div>
+      <h1>I'm step three</h1>
+      <button onClick={goNext}>Next</button>
+    </div>
+  )
+}
+function App() {
+
+  return (
+    <>
+      <UncontrolledFlow>
+        <StepOne/>
+        <StepTwo/>
+        <StepThree/>
+      </UncontrolledFlow>
+    </>
+  )
+}
+
+
 
 export default App
---
+++ uncontrolled-controlled-components/src/components/UncontrolledFlow.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+export const UncontrolledFlow = ({ children, onDone }) => {
+    const [data, setData] = useState();
+    const [currentStepIndex, setCurrentStepIndex] = useState(0);
+
+    const goNext = () => {
+        setCurrentStepIndex((prevIndex) => prevIndex + 1);
+    }
+
+    const currentChild = React.Children.toArray(children)[currentStepIndex];
+
+    if (React.isValidElement(currentChild)) {
+
+        return React.cloneElement(currentChild, { goNext });
+    }
+
+    return currentChild;
+
+}
